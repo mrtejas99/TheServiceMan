ff268e2db28dc9041f705f727dfef89fd7f43fb3
Narayan <narayanband1356@gmail.com>
2022-05-30 10:34:54 +0530

    Added user and serviceads admin panels

--- frontend/src/AdminPanel.js
+++ frontend/src/AdminPanel.js
@@ -6,7 +6,10 @@ import React from 'react';
 
 import { Admin, Resource } from 'react-admin';
 import { dataProvider, authProvider } from './Admin/AdminProvider';
-import AdminLoginPage from './Admin/AdminLoginPage'
+import AdminLoginPage from './Admin/AdminLoginPage';
+
+import * as User from './Admin/ResourceUsers';
+import * as Ad from './Admin/ResourceAds';
 
 function AdminPanel() {
 	return (
@@ -14,7 +17,8 @@ function AdminPanel() {
 				dataProvider={dataProvider}
 				authProvider={authProvider}
 				loginPage={AdminLoginPage} >
-			<Resource name="users" />
+			<Resource name="users" list={User.UserList} show={User.UserShow} edit={User.UserEdit} />
+			<Resource name="serviceads" list={Ad.AdList} show={Ad.AdShow} edit={Ad.AdEdit} />
 		</Admin>
 	);
 }
