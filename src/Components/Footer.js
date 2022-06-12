d63c25385a6ac9b6ae293f006f18b507eb50a901
Narayan <narayanband1356@gmail.com>
2022-06-12 14:01:11 +0530

    Fix footer text alignment

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,9 +7,10 @@ function Footer(){
     const {t} = useTranslation("common");
 
     return(
-        <footer className="footer text-center p-3 border-top">
-                <Button className="float-left" variant="danger" href="mailto://admin@theserviceman.com?subject=Issue regarding TheServiceMan web">{t('Report issue')}</Button><br/>
-            <div> © {t('theserviceman')} </div>
+        <footer className="footer p-3 border-top">
+            <Button variant="danger" href="mailto://admin@theserviceman.com?subject=Issue regarding TheServiceMan web">{t('Report issue')}</Button>
+            <br/>
+            <div className="text-center"> © {t('theserviceman')} </div>
         </footer>
     );
 }
