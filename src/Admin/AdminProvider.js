8f6557dc545546fa8886606057c0ea4d23add342
Narayan <narayanband1356@gmail.com>
2022-07-12 00:18:12 +0530

    Fixed admin auth from main app vulnerability

--- src/Admin/AdminProvider.js
+++ src/Admin/AdminProvider.js
@@ -32,27 +32,41 @@ const dataProvider = FirebaseDataProvider(firebaseConfig, dataOptions);
 //Default auth provider. DON'T USE THIS, ANYONE CAN AUTHENTICATE AS ADMIN!!
 const authProvider = FirebaseAuthProvider(firebaseConfig, authOptions);
 
+/* Makes sure the user currently logged in has Admin privileges */
+const validateAdmin = async () => {
+	const claims = await authProvider.getPermissions();
+
+	//User has to have "admin" or "superadmin" role(s)
+	const userRoles = claims.roles || null;
+	const isAdmin = Array.isArray(userRoles) && (userRoles.includes("admin") || userRoles.includes("superadmin"));
+
+	//If the user is actually assigned the 'admin' role
+	if (isAdmin)
+		return true;
+
+	/* ---- Not Admin ---- */
+
+	// Make sure user is logged out, if not an Admin
+	await authProvider.logout();
+	throw new Error("Login error: Invalid permissions");
+};
+
 /* Auth provider with validation for Admin privileges */
 const adminAuthProvider = {
 	...authProvider,
 	// Wrap the login and check for custom claims
 	login: async (params) => {
 		const user = await authProvider.login(params);
-		const claims = await authProvider.getPermissions();
-
-		//User has to have "admin" or "superadmin" role(s)
-		const userRoles = claims.roles || null;
-		const isAdmin = Array.isArray(userRoles) && (userRoles.includes("admin") || userRoles.includes("superadmin"));
-
-		//If the user is actually assigned the 'admin' role
+		const isAdmin = await validateAdmin();
 		if (isAdmin)
 			return user;
-
-		/* ---- Not Admin ---- */
-
-		// Make sure user is logged out, if not an Admin
-		await authProvider.logout();
-		throw new Error("Login error: Invalid permissions");
+		//Not admin, return nothing
+	},
+	checkAuth: async() => {
+		const claims = await authProvider.getPermissions();
+		const isAdmin = await validateAdmin();
+		if (isAdmin)
+			return await authProvider.checkAuth();
 	}
 };
 
